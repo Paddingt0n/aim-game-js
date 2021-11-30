74518491505c0fd4b673281d09c9502a0a25f1b4
Max Basharin <max-basarin@ya.ru>
2021-11-30 22:29:36 +0300

    Завершение основного функционала, добавлены комментарии к коду

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let $time = document.querySelector("#time");
 let $result = document.querySelector("#result");
 let $timeHeader = document.querySelector("#time-header");
 let $resultHeader = document.querySelector("#result-header");
+let $gameTime = document.querySelector("#game-time");
 
 let score = 0;
 let isGameStarted = false;
@@ -13,71 +14,88 @@ let isGameStarted = false;
 $start.addEventListener("click", startGame);
 $game.addEventListener("click", handleBoxClick);
 
-// старт игры
+// обработчик события ввода времени игры
+$gameTime.addEventListener("input", setGameTime);
+
+// функция show удаляет класс hide
+function show($el) {
+  $el.classList.remove("hide");
+}
+// функция hide добавляет класс hide
+function hide($el) {
+  $el.classList.add("hide");
+}
+
+// начало игры
 function startGame() {
   score = 0;
   setGameTime();
-  isGameStarted = true;
-  $timeHeader.classList.remove("hide");
-  $resultHeader.classList.add("hide");
+  $gameTime.setAttribute("disabled", "true"); // запрет изменения значения времени игры при запуске игры
   isGameStarted = true;
   $game.style.backgroundColor = "#fff";
-  $start.classList.add("hide");
+  hide($start);
 
+  // функция получает значение времени и сравнивает,
   let interval = setInterval(function () {
     let time = parseFloat($time.textContent);
-
+    //если больше равно 0 очищает интервал и останваливает игру
     if (time <= 0) {
       clearInterval(interval);
       endGame();
     } else {
-      $time.textContent = (time - 0.1).toFixed(1); // toFixed уберает все знаки кроме 1
+      // получает текстовые данные и оставляет 1 знак после запятой
+      $time.textContent = (time - 0.1).toFixed(1);
     }
-    console.log("interval", $time.textContent);
-  }, 100);
+  }, 100); //время обновления 100мс
 
   renderBox();
 }
 
+// отрисовывает результат
 function setGameScore() {
   $result.textContent = score.toString();
 }
 
 function setGameTime() {
-  let time = 5;
+  let time = +$gameTime.value;
   $time.textContent = time.toFixed(1);
+  show($timeHeader);
+  hide($resultHeader);
 }
 
 function endGame() {
   isGameStarted = false;
   setGameScore();
-  $start.classList.remove("hide");
+  $gameTime.removeAttribute("disabled");
+  show($start);
   $game.innerHTML = "";
   $game.style.backgroundColor = "#ccc";
-  $timeHeader.classList.add("hide");
-  $resultHeader.classList.remove("hide");
+  hide($timeHeader);
+  show($resultHeader);
 }
 
+// функция проверяет произошл ли клик по объекту
 function handleBoxClick(event) {
   if (!isGameStarted) {
     return;
   }
-
+  // если в поле dataset при клике присутствует объект box
   if (event.target.dataset.box) {
     score++;
     renderBox();
   }
 }
 
-// создание квадрата
+// функция рендера объекта
 function renderBox() {
-  $game.innerHTML = ""; // очищает поле от предыдущих объектов
-  var box = document.createElement("div");
-  var boxSize = getRandom(30, 100);
-  var gameSize = $game.getBoundingClientRect();
-  var maxTop = gameSize.height - boxSize;
-  var maxLeft = gameSize.width - boxSize;
+  $game.innerHTML = ""; //
+  let box = document.createElement("div");
+  let boxSize = getRandom(30, 100);
+  let gameSize = $game.getBoundingClientRect();
+  let maxTop = gameSize.height - boxSize;
+  let maxLeft = gameSize.width - boxSize;
 
+  box.style.borderRadius = "50%";
   box.style.height = box.style.width = boxSize + "px";
   box.style.position = "absolute";
   box.style.backgroundColor = "#000";
